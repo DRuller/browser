4796d310cfee916927c48b61e0fd72310cfb28ad
alexey <haginarovitch@gmail.com>
2018-10-02 10:48:51 +0300

    Update: added response model entity

---
+++ src/Model/Entity/Response.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rangeray
+ * Date: 01.10.18
+ * Time: 21:55
+ */
+
+namespace Browser\Model\Entity;
+
+
+use Cake\ORM\Entity;
+
+class Response extends Entity
+{
+
+}
